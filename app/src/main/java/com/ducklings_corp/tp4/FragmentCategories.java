e343ddd10a28e60f837e015d268ad95871e6da1f
Android Studio <devandroid@ort>
2019-06-11 10:40:31 -0300

    Created fragment categories

---
+++ app/src/main/java/com/ducklings_corp/tp4/FragmentCategories.java
@@ -0,0 +1,128 @@
+package com.ducklings_corp.tp4;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.AndroidException;
+import android.util.JsonReader;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class FragmentCategories extends Fragment {
+    public ArrayList<String> categories;
+    ArrayAdapter<String> categoriesAdapter;
+    View view;
+    Context _hostActivityContext;
+
+    public FragmentCategories() {
+
+    }
+    public FragmentCategories(Context hostActivityContext) {
+
+        _hostActivityContext = hostActivityContext;
+    }
+
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        Bundle arguments;
+        arguments = getArguments();
+
+            view = layoutInflater.inflate(R.layout.categories_fragment,viewGroup,false);
+
+
+        categories= new ArrayList<>();
+        categoriesAdapter= new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,categories);
+
+//        (new GetCategories()).execute();
+
+
+        return view;
+    }
+
+
+    private class GetCategories extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            URL url;
+            HttpURLConnection cnx;
+            try {
+                url = new URL("http://epok.buenosaires.gob.ar/getCategorias/");
+                cnx = (HttpURLConnection) url.openConnection();
+                Log.d("EPOK", "Cnx");
+                if (cnx.getResponseCode() == 200) {
+                    InputStream body;
+                    InputStreamReader reader;
+
+                    Log.d("EPOK", "200 OK");
+
+                    body = cnx.getInputStream();
+                    reader = new InputStreamReader(body, "UTF-8");
+                    streamToJson(reader);
+                } else {
+                    Log.d("EPOK", "Non 200 code");
+                }
+                cnx.disconnect();
+            } catch (Exception error) {
+                Log.d ("EPOK",error.getMessage());
+            }
+            return null;
+        }
+        protected void onPostExecute(Void v) {
+            super.onPostExecute(v);
+
+            ListView listView = view.findViewById(R.id.categoriesList);
+
+        }
+    }
+
+    private void streamToJson(InputStreamReader stream) {
+        JsonReader jsonReader;
+        int categoriesDownloaded;
+
+        jsonReader = new JsonReader(stream);
+        categoriesDownloaded = -1;
+
+        try {
+            jsonReader.beginObject();
+
+            jsonReader.nextName();//cantidad_de_categorias
+            categoriesDownloaded = jsonReader.nextInt();
+            Log.d("EPOK",String.format("Categories downloaded: %s",categoriesDownloaded));
+
+            jsonReader.nextName();//categorias
+            jsonReader.beginArray();
+            for(int i=0;i<categoriesDownloaded;i++) {
+                jsonReader.beginObject();
+                //Log.d("Json",jsonReader.nextName());
+                while (jsonReader.hasNext()) {
+                    String name = jsonReader.nextName();
+                    Log.d("Json",name);
+                    if(name.compareTo("nombre")==0) {
+                        categories.add(jsonReader.nextString());
+                        Log.d("Json","Storing name");
+                    } else {
+                        jsonReader.skipValue();
+                    }
+                }
+                jsonReader.endObject();
+            }
+            jsonReader.endArray();
+            jsonReader.endObject();
+            Log.d("Json","Finished parsing");
+        } catch(Exception e) {
+            Log.d("Json stream reader",e.toString());
+        }
+    }
+
+}
--- app/src/main/java/com/ducklings_corp/tp4/MainActivity.java
+++ app/src/main/java/com/ducklings_corp/tp4/MainActivity.java
@@ -1,13 +1,24 @@
 package com.ducklings_corp.tp4;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    FragmentManager manager;
+    FragmentTransaction transaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getFragmentManager();
+        Fragment fragmentCategories = new FragmentCategories();
+        transaction = manager.beginTransaction();
+        transaction.replace(R.id.categories,fragmentCategories);
+        transaction.commit();
     }
 }
